02c1d79bb5343d0c05a5c52ef8af45e3367d5f77
Nich Secord <nicholassecord@gmail.com>
2019-10-07 12:50:33 -0700

    eslint jest and cyress working together

--- cypress/integration/canary.spec.ts
+++ cypress/integration/canary.spec.ts
@@ -1,7 +1,6 @@
 describe('canary', () => {
     it('makes sure the default CRA page loades', () => {
-        //@ts-ignore
         cy.visit('http://localhost:3000')
-            .contains('testing cypress')
-    })
-})
+            .contains('testing cypress');
+    });
+});
