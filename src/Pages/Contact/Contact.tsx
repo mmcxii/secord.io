eb0188fccaec231d40671e37c19aca22004f5d0d
Nich Secord <nicholassecord@gmail.com>
2019-10-10 08:27:38 -0700

    added blank contact page

---
+++ src/Pages/Contact/Contact.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Props {}
+
+const Contact: React.FC<Props> = () => (
+  <>
+    <h2>Contact Me</h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id maxime debitis totam iure exercitationem?
+      Molestiae, id omnis, nihil, tempore quidem veniam eaque ipsam quis odio dolorem in praesentium aliquid
+      incidunt?
+    </p>
+  </>
+);
+
+export default Contact;
--- src/Pages/Router.tsx
+++ src/Pages/Router.tsx
@@ -2,26 +2,31 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import About from './About';
+import Contact from './Contact';
 import { Portfolio, PortfolioDetail, PortfolioProvider } from './Portfolio';
 
 interface Props {}
 
-const Router: React.FC<Props> = () => {
-  return (
-    <Switch>
-      <Route exact path='/'>
-        <About />
+const Router: React.FC<Props> = () => (
+  <Switch>
+    <Route exact path='/'>
+      <About />
+    </Route>
+
+    <Route exact path='/contact'>
+      <Contact />
+    </Route>
+
+    <PortfolioProvider>
+      <Route exact path='/portfolio'>
+        <Portfolio />
       </Route>
-      <PortfolioProvider>
-        <Route exact path='/portfolio'>
-          <Portfolio />
-        </Route>
-        <Route exact path='/portfolio/:projectSlug'>
-          <PortfolioDetail />
-        </Route>
-      </PortfolioProvider>
-    </Switch>
-  );
-};
+
+      <Route exact path='/portfolio/:projectSlug'>
+        <PortfolioDetail />
+      </Route>
+    </PortfolioProvider>
+  </Switch>
+);
 
 export default Router;
