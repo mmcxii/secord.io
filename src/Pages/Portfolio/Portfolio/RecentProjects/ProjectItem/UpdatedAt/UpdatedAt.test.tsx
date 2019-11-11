164cc436cd9841dadb73c27e35fe7e77dda0a905
Nich Secord <nicholassecord@gmail.com>
2019-11-11 13:26:38 -0800

    edit test value

--- src/Pages/Portfolio/Portfolio/RecentProjects/ProjectItem/UpdatedAt/UpdatedAt.test.tsx
+++ src/Pages/Portfolio/Portfolio/RecentProjects/ProjectItem/UpdatedAt/UpdatedAt.test.tsx
@@ -7,7 +7,7 @@ describe('<UpdatedAt /> tests', () => {
   it('formats datestrings correctly', () => {
     //* Arrange
     const testID = 1234;
-    const testValue = '2019-11-08T02:38:52Z';
+    const testValue = '2019-11-11T12:38:52Z';
 
     //* Act
     const { getByTestId } = render(<UpdatedAt updatedAt={testValue} testID={testID} />);
@@ -15,6 +15,6 @@ describe('<UpdatedAt /> tests', () => {
     const formattedDate = getByTestId(`updated-at--${testID}`);
 
     //* Assert
-    expect(getNodeText(formattedDate)).toBe('Last Updated: Thursday, Nov 7, 2019');
+    expect(getNodeText(formattedDate)).toBe('Last Updated: Monday, Nov 11, 2019');
   });
 });
