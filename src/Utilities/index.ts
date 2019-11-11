04276e5071f9d84c9d7bc77b9558b28faafe0b70
Nich Secord <nicholassecord@gmail.com>
2019-11-11 09:02:00 -0800

    added elevation and transition

--- src/Utilities/index.ts
+++ src/Utilities/index.ts
@@ -1,6 +1,8 @@
+import elevation from './elevation';
 import spacing from './spacing';
+import transition from './transition';
 
 export * from './colors';
 export * from './rounded';
 
-export { spacing };
+export { elevation, spacing, transition };
