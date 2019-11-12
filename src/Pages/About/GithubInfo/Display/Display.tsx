8f8cce1e90be1694a1a46b209c9e962c4852222e
Nich Secord <nicholassecord@gmail.com>
2019-11-12 12:42:05 -0800

    styled github info card using grid

--- src/Pages/About/GithubInfo/Display/Display.tsx
+++ src/Pages/About/GithubInfo/Display/Display.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { green, orange } from 'Utilities';
+import { green, orange, roundedInner, spacing, transition, blue } from 'Utilities';
 import { Card as C } from 'Elements';
 
 interface IGithubInfo {
@@ -20,24 +20,24 @@ interface Props {
 
 const Display: React.FC<Props> = ({ info }) => (
   <Card>
-    <p data-testid='github-name'>{info.name}</p>
-    <small>
+    <Name data-testid='github-name'>{info.name}</Name>
+    <Username>
       <a href={info.htmlUrl} target='blank' data-testid='github-link'>
-        <i className='fab fa-github' />
+        <GithubIcon className='fab fa-github' />
         {info.login}
       </a>
-    </small>
-    <p data-testid='github-bio'>{info.bio}</p>
-    <p data-testid='github-hireable'>
-      <i
+    </Username>
+    <Bio data-testid='github-bio'>{info.bio}</Bio>
+    <Hireable data-testid='github-hireable'>
+      <HireableIcon
         className={`fas ${info.hireable ? 'fa-check-circle' : 'fa-times-circle'}`}
-        style={{ color: info.hireable ? green : orange }}
+        hireable={info.hireable}
         data-testid='github-hireable-icon'
       />{' '}
       I am currently
       {!info.hireable && 'not'} looking for work{info.hireable ? '!' : '.'}
-    </p>
-    <img src={info.avatarUrl} alt={info.name} data-testid='github-picture' />
+    </Hireable>
+    <ProfilePic src={info.avatarUrl} alt={info.name} data-testid='github-picture' />
   </Card>
 );
 
@@ -47,16 +47,55 @@ const Card = styled(C).attrs({ as: 'section' })`
   grid-area: githubInfo;
 
   display: grid;
+  grid-template-columns: max-content 1fr max-content;
+  grid-template-rows: repeat(4, max-content);
+  grid-template-areas:
+  'name . username'
+  'pic pic pic'
+  'hireable hireable hireable'
+  'bio bio bio';
+  grid-gap: ${spacing.sm};
+  align-items: center;
 `;
 
-const Name = styled.p``;
+const Name = styled.p`
+grid-area: name;
 
-const Login = styled.small``;
+  font-size: 1.3rem;
+`;
 
-const Bio = styled.p``;
+const Username = styled.small`
+grid-area: username;
 
-const Hireable = styled.p``;
+  font-size: 0.9rem;
 
-const HireableIcon = styled.i``;
+  > a {
+    ${transition({ prop: 'color' })};
+    
+    &:hover {
+      color: ${blue};
+    }
+  }
+`;
 
-const ProfilePic = styled.img``;
+const GithubIcon = styled.i`
+  margin-right: ${spacing.xs};
+`;
+
+const Bio = styled.p`
+  grid-area: bio;
+`;
+
+const Hireable = styled.p`
+  grid-area: hireable;
+`;
+
+const HireableIcon = styled.i<{ hireable: boolean; }>`
+  color: ${props => props.hireable ? green : orange};
+`;
+
+const ProfilePic = styled.img`
+  grid-area: pic;
+
+  border-radius: ${roundedInner}
+`;
