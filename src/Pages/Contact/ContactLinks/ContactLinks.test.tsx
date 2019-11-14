19e83dd461a587720841153ca5595851ab2bc936
Nich Secord <nicholassecord@gmail.com>
2019-11-14 09:54:12 -0800

    added links to social medias and resume

---
+++ src/Pages/Contact/ContactLinks/ContactLinks.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
+import ContactLinks from './ContactLinks';
+
+describe('<ContactLinks /> tests', () => {
+  it('has all contact info', () => {
+    //* Act
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <ContactLinks />
+      </BrowserRouter>
+    );
+
+    //* Assert
+    const emailLink = getByTestId('link-email');
+    const githubLink = getByTestId('link-github');
+    const linkedinLink = getByTestId('link-linkedin');
+    const twitterLink = getByTestId('link-twitter');
+    const resumeLink = getByTestId('link-resume');
+
+    expect(emailLink.getAttribute('href')).toBe('mailto:nich@secord.io');
+    expect(githubLink.getAttribute('href')).toBe('https://www.github.com/mmcxii');
+    expect(linkedinLink.getAttribute('href')).toBe('https://www.linkedin.com/in/nichsecord');
+    expect(twitterLink.getAttribute('href')).toBe('https://www.twitter.com/nichsecord');
+    expect(resumeLink.getAttribute('href')).toBe('/resume');
+  });
+});
--- src/Pages/Contact/ContactLinks/ContactLinks.tsx
+++ src/Pages/Contact/ContactLinks/ContactLinks.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {}
 
 const ConctactLinks: React.FC<Props> = () => {
-  return <></>;
+  const links: { testId: string; href: string; icon: string; text: string }[] = [
+    {
+      testId: 'email',
+      href: 'mailto:nich@secord.io',
+      icon: 'fas fa-envelope',
+      text: 'nich@secord.io'
+    },
+    {
+      testId: 'github',
+      href: 'https://www.github.com/mmcxii',
+      icon: 'fab fa-github',
+      text: 'mmcxii'
+    },
+    {
+      testId: 'linkedin',
+      href: 'https://www.linkedin.com/in/nichsecord',
+      icon: 'fab fa-linkedin-in',
+      text: 'nichsecord'
+    },
+    {
+      testId: 'twitter',
+      href: 'https://www.twitter.com/nichsecord',
+      icon: 'fab fa-twitter',
+      text: 'nichsecord'
+    }
+  ];
+
+  return (
+    <section>
+      {links.map(item => (
+        <a href={item.href} data-testid={`link-${item.testId}`} key={item.testId}>
+          <i className={item.icon} />
+          {item.text}
+        </a>
+      ))}
+      <Link to='/resume' data-testid='link-resume'>
+        <i className='fas fa-file-user' />
+        Resume
+      </Link>
+    </section>
+  );
 };
 
 export default ConctactLinks;
